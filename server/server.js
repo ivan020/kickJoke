00a126d2acd7d45b6a5a3b0dfc00e96c94cffeed
ivan <ivevdm@gmail.com>
2026-01-20 16:47:06 +0000

    prepare for the gh deployment

--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api/joke", (req, res) => {
-  res.json({ joke: getRandomJoke() });
+  try {
+    res.json({ joke: getRandomJoke() });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to get joke" });
+  }
 });
 
 app.listen(PORT, () => {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import "./style.css";
 
+const API_URL ="https://kickjoke.onrender.com/api/joke";
 
 async function appendJoke() {
-  const res = await fetch("/api/joke");
+  const res = await fetch(API_URL);
   const data = await res.json();
   document.getElementById("joke").textContent = data.joke;
 }
-
 appendJoke();
 
 document
